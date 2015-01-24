617a5093f0707ac4fa12d7103a46af2b64e4c44a
JMNSY <moses1991@gmail.com>
2015-01-25 01:27:57 +0800

    Update dmhyBangumiAddLink.user.js

--- dmhyBangumiAddLink.user.js
+++ dmhyBangumiAddLink.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       dmhyBangumiAddLink
 // @namespace  http://dmhyBangumiAddLink.JMNSY/
-// @version    0.1
+// @version    0.2
 // @description  番组表的图片监听左键单击，直接在当前页跳转不符合本人浏览习惯，故对每一张图片覆盖上一个链接元素
 // @match      http://share.dmhy.org/cms/page/name/programme.html
 // @copyright  2015.01.25, JMNSY
@@ -9,9 +9,8 @@
 jQuery().ready(function(){
     jQuery(".weekly_list_b").each(function(){
         var now = jQuery(this);
-        var temp = jQuery(this).clone();
-        var href = temp.attr("onclick").toString().match(/(?:\/).*(?=')/);
-        jQuery("<a/>").attr("href",href).append(temp.removeAttr("onclick")).insertAfter(now);
+        var temp = now.clone();
+        jQuery("<a/>").attr("href",temp.attr("onclick").toString().match(/(?:\/).*(?=')/)).append(temp.removeAttr("onclick")).insertAfter(now);
         now.remove();
     });
 });
